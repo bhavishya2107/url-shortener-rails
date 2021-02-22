52abe85865c33c494218eb2f598dc318b6f18490
bhavishya2107 <bhavishya2107@gmail.com>
2021-02-22 20:52:37 +0530

    removed few components , setup app complete

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/" render={() => <div>Home</div>} />
+    //     <Route exact path="/about" render={() => <div>About</div>} />
+    //   </Switch>
+    // </Router>
+    <h1 className="text-2xl text-center">This is App.jsx</h1>
   );
 };
 

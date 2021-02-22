60f7a86035957a755a2a80d8e43865da625509e0
bhavishya2107 <bhavishya2107@gmail.com>
2021-02-23 00:47:20 +0530

    Add action to create url and methods to create slug and default pinned value to be false

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,17 @@
 class Url < ApplicationRecord
+  before_validation :generate_slug, :set_defaults
+
+  validates :original_url, presence: true, format: URI::regexp(%w[http https])
+  validates :slug, presence: true, uniqueness: true
+
+  def generate_slug
+    loop do
+      self.slug = [*("a".."z"), *("0".."9")].shuffle[0, 6].join
+      break slug unless Url.where(slug: self.slug).exists?
+    end
+  end
+
+  def set_defaults
+    self.pinned = false if self.new_record?
+  end
 end
